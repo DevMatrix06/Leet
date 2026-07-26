4899e742d05e10365882b467ef2e53729630a099
Tejas <tejasprasad181@gmail.com>
2026-07-26 11:18:15 +0530

    Add 242 Valid Anagram

---
+++ Strings/Valid_Anagram.cpp
@@ -0,0 +1,14 @@
+bool isAnagram(string s, string t) {
+
+  if (t.length()!=s.length()){
+  return false;}
+  int freq[26]={0};
+  for(char c:t)
+	  freq[c-'a']++;          //on finding a letter make the array index=1
+  for(char c:s)
+	  freq[c-'a']--;          //on finding a letter in array make it 0 back again (anagram
+  for(int i=0;i<26;i++){
+	  if(freq[i]!=0)          //if all array elements are 0 again they are anagrams
+		  return false;}
+  
+  return true;
