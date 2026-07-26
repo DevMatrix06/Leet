9bfbc4af7efa8d2989e78ccbbd3481be429b3f40
Tejas P <tejasprasad181@gmail.com>
2026-07-26 11:44:47 +0530

    Enhance code readability with comments
    Added comments to clarify the loop and condition logic.

--- Strings/find_first_occurenceofSubString.cpp
+++ Strings/find_first_occurenceofSubString.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
 
-        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {                //run the for loop till difference of the two lengths os strings
 
-            if (haystack.substr(i, needle.length()) == needle)
+            if (haystack.substr(i, needle.length()) == needle)                          //if substring of the string exists return i index
                 return i;
         }
 
