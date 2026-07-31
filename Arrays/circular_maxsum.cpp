d9e191ebd97a7c4a86eaa04bad7bf2ce47cbd90a
Tejas <tejasprasad181@gmail.com>
2026-07-31 17:25:10 +0530

    For circular maxsum just do total-minsum

---
+++ Arrays/circular_maxsum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int maxsum=nums[0];
+        int maxEnd=nums[0];
+        int minsum=nums[0];
+        int minEnd=nums[0];
+        int n=nums.size();
+        int total=nums[0];
+        for(int i=1;i<n;i=i+1){
+            total+=nums[i];
+            maxEnd=max(nums[i],nums[i]+maxEnd);
+            maxsum=max(maxsum,maxEnd);
+
+            minEnd=min(nums[i],nums[i]+minEnd);
+            minsum=min(minsum,minEnd);
+        }
+        if(maxsum<0){
+            return maxsum;
+        }
+        return max(maxsum,total-minsum);
+        
+    }
+};
