0a96e0adda462492cf757c1844e710100577e4e9
Tejas P <tejasprasad181@gmail.com>
2026-08-02 19:21:04 +0530

    Improve comments in isAnagram function
    Added comments to clarify logic for checking anagrams.

--- Strings/Valid_Anagram.cpp
+++ Strings/Valid_Anagram.cpp
@@ -1,8 +1,8 @@
 bool isAnagram(string s, string t) {
 
   if (t.length()!=s.length()){
-  return false;}
-  int freq[26]={0};
+  return false;}                           //unequal lengths cant be anagram
+  int freq[26]={0};                    //initialise everything to 0
   for(char c:t)
 	  freq[c-'a']++;          //on finding a letter make the array index=1
   for(char c:s)
