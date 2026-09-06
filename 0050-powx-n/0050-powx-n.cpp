b121a82ad65a1c2f53313e02a0b3eb998355efeb
Tejas P <tejasprasad181@gmail.com>
2026-09-06 21:56:28 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.82%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long bin=n;
+        double ans=1;
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return x;
+        }
+        if(n<0){
+            x=1/x;
+            bin=-bin;
+        }
+        while(bin>0){
+            if(bin%2==1){
+                ans=ans*x;
+            }
+            x=x*x;
+            bin/=2;
+        }
+        return ans;
+        
+    }
+};
