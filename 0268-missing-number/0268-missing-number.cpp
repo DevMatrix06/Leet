ba55be346b3f7c4e1871fc0d7f99291dc6a58a22
Tejas P <tejasprasad181@gmail.com>
2026-09-10 23:08:56 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.26%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,18 +3,12 @@ public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
         vector<int>arr1;
-        for(int i=0;i<n;i++){
-            arr1.push_back(i);
+        int total=0;
+        int sum=n*(n+1)/2;
+        for(auto x:nums){
+            total+=x;
+
         }
-    
-    int missing;
-    int j=0;
-    sort(nums.begin(),nums.end());
-    while(j<n){
-        if(nums[j]!=j)
-        return j;
-        j++;
-    }
-        return n;
-    }
-};
+        return sum-total;
+        
+}};
